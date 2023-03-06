520a8672ccd2671af3c073eaea02537688f3883d
Krish0799 <makewithkrish@gmail.com>
2023-03-06 17:18:17 +0530

    Updated

--- Conditions and Loops/2 sum of Numbers.py
+++ Conditions and Loops/2 sum of Numbers.py
@@ -1,3 +1,19 @@
+'''
+Given an integer n, find and print the sum of numbers from 1 to n.
+Note
+Use while loop only.
+Input Format :
+Integer n
+Output Format :
+Sum of numbers
+Constraints :
+1 <= n <= 100
+Sample Input :
+10
+Sample Output :
+55
+'''
+
 # Read input as sepcified in the question
 # Print output as specified in the question
 n = int(input())
--- Conditions and Loops/7 Palindrome Number.py
+++ Conditions and Loops/7 Palindrome Number.py
@@ -1,3 +1,15 @@
+'''
+Write a program to determine if given number is palindrome or not. Print true if it is palindrome, false otherwise.
+Palindrome are the numbers for which reverse is exactly same as the original one. For eg. 121
+Sample Input 1 :
+121
+Sample Output 1 :
+true
+Sample Input 2 :
+1032
+Sample Output 2 :
+false
+'''
 def checkPalindrome(num):
     res=0
     t=num
